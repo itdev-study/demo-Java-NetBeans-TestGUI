2a5b8ad1031e50d1a5c2a8e419fa5ecb376c7e36
o.levitskiy <o.levitskiy@GR-MS-IT03.ipmatika.local>
2018-04-05 16:43:05 +0300

    FirstAlfa-0.1

--- Java-NetBeans-TestGUI/src/demo/java/netbeans/testgui/JavaNetBeansTestGUI.java
+++ Java-NetBeans-TestGUI/src/demo/java/netbeans/testgui/JavaNetBeansTestGUI.java
@@ -16,6 +16,7 @@ public class JavaNetBeansTestGUI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         System.out.println("Test project starred");
     }
     
 }
